7e019886f6a8760427cae56078733d7dd3981458
348775859-wq <348775859@gapps.yrdsb.ca>
2026-05-28 11:31:48 -0400

    pokemon constructor

--- Pokemon Battle Simulator/core/src/main/java/com/mygame/battlesimulator/Pokemon.java
+++ Pokemon Battle Simulator/core/src/main/java/com/mygame/battlesimulator/Pokemon.java
@@ -1,5 +1,43 @@
 package com.mygame.battlesimulator;
 
 public class Pokemon {
+    private String name;
+    private int health;
+    private int attack;
+    private int defense;
+    private int specialAttack;
+    private int specialDefense;
+    private int speed;
+    private String type1;
+    private String type2;
+    private String userSprite;
+    private String opponentSprite;
+    private String moves;
+    private int width;
+    private int height;
+    private int frontFrames;
+    private int backFrames;
+
+
+    public void constructor(name, health, attack, defense, specialAttack, specialDefense, speed, type1, type2, userSprite, opponentSprite, moves, width, height, frontFrames, backFrames)
+    {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+        this.specialAttack = specialAttack;
+        this.specialDefense = specialDefense;
+        this.speed = speed;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.userSprite = userSprite;
+        this.opponentSprite = opponentSprite;
+        this.moves = moves;
+        this.width = width;
+        this.height = height;
+        this.frontFrames = frontFrames;
+        this.backFrames = backFrames;
+    }
     
+
 }
