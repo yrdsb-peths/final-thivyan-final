fca58a0ba315381187b910ddb454f58f0fe44d4e
348775859-wq <348775859@gapps.yrdsb.ca>
2026-06-01 11:45:27 -0400

    Creating move class

--- Pokemon Battle Simulator/core/src/main/java/com/mygame/battlesimulator/Move.java
+++ Pokemon Battle Simulator/core/src/main/java/com/mygame/battlesimulator/Move.java
@@ -1,5 +1,44 @@
 package com.mygame.battlesimulator;
 
-public class Move {
 
+import com.badlogic.gdx.utils.JsonValue;
+
+public class Move {
+    //private String name;
+    private String type;
+    private String category;
+    private int power;
+    private int accuracy;
+    private int points;
+
+
+    public Move(JsonValue data)
+    {
+        this.type = data.getString("type");
+        this.category = data.getString("category");
+
+        this.power = data.getInt("hp");
+        this.accuracy = data.getInt("attack");
+        this.points = data.getInt("pp");
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getAccuracy() {
+        return accuracy;
+    }
+
+    public int getPoints() {
+        return points;
+    }
 }
