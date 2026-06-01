1fdb271e4d4c9efb68d8b4b1446cae48220dff9c
348775859-wq <348775859@gapps.yrdsb.ca>
2026-06-01 11:56:55 -0400

    Damage Calculator

--- Pokemon Battle Simulator/core/src/main/java/com/mygame/battlesimulator/DamageCalc.java
+++ Pokemon Battle Simulator/core/src/main/java/com/mygame/battlesimulator/DamageCalc.java
@@ -1,5 +1,34 @@
 package com.mygame.battlesimulator;
 
-public class DamageCalc {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
 
+public class DamageCalc {
+    private int damage;
+
+    public int calculateDamage(Pokemon attacker, Pokemon defender, Move move)
+    {
+
+        if (move.getCategory().equals("Physical"))
+        {
+            double baseAttack = attacker.getAttack();
+            double movesAttack = move.getPower();
+            double defense = defender.getDefense();
+            double pureDamage = (0.84 * ((baseAttack * movesAttack)/defense)) + 2;
+            // Rounds damage
+            damage = (int) pureDamage;
+        }
+        else if (move.getCategory().equals("Special"))
+        {
+            double baseAttack = attacker.getSpecialAttack();
+            double movesAttack = move.getPower();
+            double defense = defender.getSpecialDefense();
+            double pureDamage = (0.84 * ((baseAttack * movesAttack)/defense)) + 2;
+            // Rounds damage
+            damage = (int) pureDamage;
+        }
+
+        return damage;
+    }
 }
